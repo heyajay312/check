475309ef422286c729665d311c0523e6c176c260
Ajay Yadav <134627590+heyajay312@users.noreply.github.com>
2024-01-20 14:30:26 +0530

    Update __init__.py

--- find_max/__init__.py
+++ find_max/__init__.py
@@ -20,19 +20,19 @@ def simple():
 @check50.check(exists)
 def negetive():
     """returns maximum from -9, -101, -53, -74"""
-    check_max(number=[-9, -101, -53, -74])
+    check_max(numbers=[-9, -101, -53, -74])
 
 
 @check50.check(exists)
 def mixed():
     """returns maximum from -993, 139, 22, -45"""
-    check_max(number=[-993, 139, 22, -45])
+    check_max(numbers=[-993, 139, 22, -45])
 
 
 def check_max(numbers: list):
     code = check50.run("./max")
-    code.stdin(str(len(number)))
-    for i in number:
-        code.stdin(str(number[i]))
-    program.stdout(str(max(number)))
+    code.stdin(str(len(numbers)))
+    for i in numbers:
+        code.stdin(str(numbers[i]))
+    program.stdout(str(max(numbers)))
     
