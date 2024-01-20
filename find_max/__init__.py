7746f71a3f6bd68129174f821f1e8b26406fe2a7
Ajay Yadav <134627590+heyajay312@users.noreply.github.com>
2024-01-20 13:23:04 +0530

    Update __init__.py

--- find_max/__init__.py
+++ find_max/__init__.py
@@ -1,41 +1,38 @@
 import check50
 import check50.c
 
-
-@check50.check()
-def exists():
+@check50.check():
+def exists:
     """max.c exists"""
-    check50.exists("max.c")
+    check50.exist ("max.c")
+
+@check50.check(exists)
+def compiles:
+    """max.c compiles"""
+    check50.c.compile("max.c")
+
+@check50.check(exists)
+def simple:
+    """returns maximum from 3, 19, 7"""
+    check_max(numbers=[3, 19, 7])
 
 
 @check50.check(exists)
-def compiles():
-    """max.c compiles"""
-    check50.c.compile("max.c", lcs50=True)
+def negetive:
+    """returns maximum from -9, -101, -53, -74"""
+    check_max(number=[-9, -101, -53, -74])
 
 
-@check50.check(compiles)
-def simple():
-    """Returns 3 from 0, 1, 3"""
-    check_max(elements=[0, 1, 3])
+@check50.check(exits)
+def mixed:
+    """returns maximum from -993, 139, 22, -45"""
+    check_max(number=[-993, 139, 22, -45])
 
 
-@check50.check(compiles)
-def negative():
-    """Returns 4 from -10, 4, 2"""
-    check_max(elements=[-10, 4, 2])
-
-
-@check50.check(compiles)
-def negative_max():
-    """Returns -10 from -10, -50, -100"""
-    check_max(elements=[-10, -50, -100])
-
-
-# Helpers
-def check_max(elements: list):
-    program = check50.run("./max")
-    program.stdin(str(len(elements)))
-    for number in elements:
-        print(program.stdin(str(number)))
-    program.stdout(str(max(elements)))
+def check_max(numbers: list):
+    code = check50.run("./max")
+    code.stdin(str(len(number)))
+    for i in number:
+        code.stdin(str(number[i]))
+    program.stdout(str(max(number)))
+    
