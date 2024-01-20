63c7124c7874e5a4d6129e3372ab9ccb1128b4de
Ajay Yadav <134627590+heyajay312@users.noreply.github.com>
2024-01-20 16:15:16 +0530

    Update __init__.py

--- find_max/__init__.py
+++ find_max/__init__.py
@@ -14,28 +14,28 @@ def compiles():
     check50.c.compile("max.c", lcs50=True)
 
 
-@check50.check(exists)
+@check50.check(compiles)
 def simple():
-    """returns maximum from 3, 7, 19"""
-    check_max(numbers=[3, 7, 19])
+    """Returns 3 from 0, 1, 3"""
+    check_max(elements=[0, 1, 3])
 
 
-@check50.check(exists)
-def negetive():
-    """returns maximum from -9, -101, -53, -74"""
-    check_max(numbers=[-9, -101, -53, -74])
+@check50.check(compiles)
+def negative():
+    """Returns 4 from -10, 4, 2"""
+    check_max(elements=[-10, 4, 2])
 
 
-@check50.check(exists)
-def mixed():
-    """returns maximum from 993, -139, 22, -45"""
-    check_max(numbers=[993, -139, 22, -45])
+@check50.check(compiles)
+def negative_max():
+    """Returns -10 from -10, -50, -100"""
+    check_max(elements=[-10, -50, -100])
 
 
-def check_max(numbers: list):
+# Helpers
+def check_max(elements: list):
     program = check50.run("./max")
-    program.stdin(str(len(numbers)))
-    for i in numbers:
-        program.stdin(str(i))
-    program.stdout(str(max(numbers)))
-    
+    program.stdin(str(len(elements)))
+    for number in elements:
+        print(program.stdin(str(number)))
+    program.stdout(str(max(elements)))
